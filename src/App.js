17bc7b43c84f0857a2eea6e7fe13977569196be8
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2019-12-22 19:26:58 +0900

    UPDATE App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello world!</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
