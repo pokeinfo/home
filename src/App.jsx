c13f89c1800afd2068c5f896228c4c443f5738d5
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-01-08 23:23:17 +0900

    Move App.css :arrow_right: css/App.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import IVChecker from './components/IVChecker/IVChecker';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
