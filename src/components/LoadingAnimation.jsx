afebfd0712cd5aae7fad48f653314f9342624799
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-01-21 00:30:15 +0900

    Pokeball image => svg + css

--- src/components/LoadingAnimation.jsx
+++ src/components/LoadingAnimation.jsx
@@ -3,15 +3,19 @@ import Container from './Container';
 import { CenteredText } from './Text';
 import styles from '../css/components/LoadingAnimation.module.scss'
 
-import pokeballImage from '../img/pokeball.png';
+import { ReactComponent as EmptyPokeballImage } from '../svg/pokeball.svg';
+
+const PokeballImage = () => (
+  <div className={styles.pokeball}>
+    <div />
+    <div><div></div></div>
+    <EmptyPokeballImage />
+  </div>
+);
 
 const LoadingAnimation = () => (
   <Container>
-    <img
-      src={pokeballImage}
-      alt="Loading spinning Pokèball"
-      className={styles.loadingRing}
-    />
+    <PokeballImage />
     <br />
     <CenteredText>불러오는 중...</CenteredText>
   </Container>
