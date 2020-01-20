e9d4818bbc7ff1310cf36b3383ffe2374a40fc92
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-01-20 20:54:11 +0900

    CREATE Text component

---
+++ src/components/Text.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+import styles from '../css/components/Text.module.scss';
+
+const Text = ({...rest}) => (
+  <p {...rest} />
+);
+
+const CenteredText = ({
+  className,
+  ...rest
+}) => {
+  rest.className = classNames(className, 'center');
+  return (
+    <Text {...rest} />
+  );
+}
+
+const VerticalCenterText = ({
+  className,
+  ...rest
+}) => {
+  rest.className = classNames(className, styles.verticalCenter);
+  return (
+    <Text {...rest} />
+  );
+}
+
+export default Text;
+export {
+  CenteredText,
+  VerticalCenterText,
+};
