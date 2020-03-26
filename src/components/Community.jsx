ea32de0eb4ebb5adbfc6f5599be9292552a726c6
tmdgus0084 <32220765+tmdgus0084@users.noreply.github.com>
2020-03-26 15:23:05 +0900

    UPDATE Disqus component
    Add comment loding message.
    
    https://github.com/disqus/disqus-react/pull/63#issuecomment-604235411

--- src/components/Community.jsx
+++ src/components/Community.jsx
@@ -6,7 +6,9 @@ import Disqus from "disqus-react";
 const Community = () => (
   <Container maxWidth={650}>
     <Title>Pokè Info</Title>
-    <Disqus.DiscussionEmbed shortname="poke-info" config={{}} />
+    <Disqus.DiscussionEmbed shortname="poke-info" config={{}}>
+      댓글을 불러오는 중...
+    </Disqus.DiscussionEmbed>
   </Container>
 );
 
